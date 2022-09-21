0b2b4abd03475dcf39d8ad2aaa78a9f3f25b6129
andronik <90332364+andronik-GitHub@users.noreply.github.com>
2022-09-21 07:50:08 +0300

    Add files via upload

---
+++ Ex9/Ex9.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Ex9
+{
+    static void Main()
+    {
+        Console.Write("N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("M: ");
+        int M = Convert.ToInt32(Console.ReadLine());
+
+        Rectangle[] rectangles = new Rectangle[N];
+
+        for (int i = 0; i < N; i++)
+            if (rectangles[i] == null)
+                rectangles[i] = new Rectangle();
+
+
+        Console.WriteLine();
+        rectangles[0].Input(rectangles);
+
+
+        for (int i = 0; i < N && M > 0; i++)
+            for (int j = 0; j < N - i && M > 0; j++)
+                if (rectangles[i].Check(rectangles[j]) && j != i)
+                {
+                    Console.WriteLine($"{rectangles[i].id} {rectangles[j].id}");
+                    M--;
+                }
+
+
+        Console.ReadKey();
+    }
+}
---
+++ Ex9/Rectangle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Rectangle
+{
+    public string? id;
+    public float? width;
+    public float? height;
+    public float? X;
+    public float? Y;
+
+    public void Input(Rectangle[] Array)
+    {
+        for (int i = 0; i < Array.Length; i++)
+        {
+            if (Array[i] == null)
+                Array[i] = new Rectangle();
+
+            Console.Write("id: ");
+            Array[i].id = Console.ReadLine();
+
+
+            Console.Write("Width: ");
+            Array[i].width = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Height: ");
+            Array[i].height = Convert.ToSingle(Console.ReadLine());
+
+
+            Console.Write("X: ");
+            Array[i].X = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Y: ");
+            Array[i].Y = Convert.ToSingle(Console.ReadLine());
+
+
+            Console.WriteLine();
+        }
+    }
+
+    public bool Check(Rectangle rectangle)
+    {
+        if ((rectangle.X >= X && rectangle.Y >= Y && rectangle.X <= X + width && rectangle.Y <= Y + height) ||
+
+            (rectangle.X + rectangle.width >= X && rectangle.Y >= Y && rectangle.X + rectangle.width <= X + width && rectangle.Y <= Y + height) ||
+
+            (rectangle.X >= X && rectangle.Y + rectangle.height >= Y && rectangle.X <= X + width && rectangle.Y + rectangle.height <= Y + height) ||
+
+            (rectangle.X + rectangle.width >= X && rectangle.Y + rectangle.height >= Y && 
+            rectangle.X + rectangle.width <= X + width && rectangle.Y + rectangle.height <= Y + height))
+            return true;
+
+        return false;
+    }
+
+    public Rectangle()
+    {
+        id = new string("");
+
+        X = Y = height = width = 0.0f;
+        
+    }
+}
